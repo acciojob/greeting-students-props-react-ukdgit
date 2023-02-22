1fc4118ec6e92e69c0c302909b36526a528e97cc
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-22 21:09:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Welcome from './Welcome';
+import Welcome from "./Welcome"
 
 const App = () => {
   return (
-    <Welcome name="" />
+    <Welcome name=""></Welcome>
   )
 }
-
 
 export default App;
